88c5984a9a84a360cb14cfb52882ca93d871fbed
Samat K Jain <samat@samat.org>
2017-03-09 19:14:51 -0700

    Simplify and comment CoL calculations

--- col_figurin.php
+++ col_figurin.php
@@ -2,37 +2,36 @@
 
 // COL = Cost of Living
 
+/**
+ * Inputs from cl_results.php:
+ * • HowManyAdults
+ * • HowManyChildren
+ * • cl_county
+ * • cl_state
+ * • $cl_input['cl_adults_FT']
+ */
+
 //LOOKUP COST OF LIVING AMOUNTS BASED ON FAMILY COMPOSITION
-if(isset($HowManyAdults)) {
-	$adults = $HowManyAdults;
-} else {
-	$adults = "0";
-}
-if(isset($HowManyChildren)) {
-	$children = $HowManyChildren;
-	if($children > 3) {
-		$children = "3";
-	}
-} else {
-	$children = "0";
-}
-if(isset($cl_input['cl_adults_FT'])) {
-	$working = "1";
-} else {
-	$working = "0";
-}
-$county = $cl_input['cl_county'];
-$state = $cl_input['cl_state'];
 
-if($adults = 1) {
+// Clamp number of children to 3
+$children = $HowManyChildren;
+if ($children > 3)
+	$children = 3;
+
+if(isset($cl_input['cl_adults_FT']))
+	$working = 1;
+else
 	$working = 0;
-}
 
-$awc = "a" . $adults . "w" . $working . "c" . $children;
+if ($HowManyAdults == 1)
+	$working = 0;
+
+// Generate lookup string
+$awc = "a" . $HowManyAdults . "w" . $working . "c" . $children;
 
 $costofliving = 0;
 
-$ea_sql = "SELECT type_name, " . $awc . " FROM expense_allowances WHERE county_name=\"" . $county . "\" AND state_name=\"" . $state . "\";";
+$ea_sql = "SELECT type_name, " . $awc . " FROM expense_allowances WHERE county_name=\"" . $cl_county . "\" AND state_name=\"" . $cl_state . "\";";
 $ea_result = $dbconn->query($ea_sql);
 while($ea_row = $ea_result->fetch_assoc()) {
 	$monthly_allow = $ea_row[$awc];
@@ -49,7 +48,7 @@ $col_RequiredGrossAnnual = $col_RequiredAnnual + $col_AnnualTaxes;
 //=(I16*9.5%)/12
 $col_Obama = ($col_RequiredGrossAnnual * .095) / 12;
 
-$medcol_sql = "SELECT " . $awc . " FROM expense_allowances WHERE county_name=\"" . $county . "\" AND state_name=\"" . $state . "\" AND type_name=\"Medical\";";
+$medcol_sql = "SELECT " . $awc . " FROM expense_allowances WHERE county_name=\"" . $cl_county . "\" AND state_name=\"" . $cl_state . "\" AND type_name=\"Medical\";";
 //echo "<br><br>MEDCOL_SQL == ".$medcol_sql."<br>";
 $medcol_result = $dbconn->query($medcol_sql);
 $medcol_row = $medcol_result->fetch_assoc();
