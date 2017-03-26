codeforabq/circles

commit 7fdfc79b77b98389c2091639d801b58179546b09

Bunch of stuff
* Start saving cl_state into sessionStorage
* Start using JSON for log file format

cl_infos.php
@@ -489,29 +489,18 @@
 				document.getElementById("ThatsAllFolks").style.display = "block";
 			}
 
+			// After the user selects a state
+			function StateSelectionSubmission() {
+				let cl_state = $('select#cl_state').val();
+				sessionStorage.setItem('cl_state', cl_state);
+				console.log(cl_state);
+			}
+
 		</script>
 
 	</head>
 
 	<body>
-		<?php
-		if(!isset($ishomeless)) {
-			$ishomeless = "No";
-		}
-		//echo "<table border=\"1\">";
-		//foreach ($_POST as $key => $value) {
-		//  echo "<tr>";
-		//  echo "<td>";
-		//  echo $key;
-		//  echo "</td>";
-		//  echo "<td>";
-		//  echo $value;
-		//  echo "</td>";
-		//  echo "</tr>";
-		//  }
-		// echo "</table>"
-		?>
-
 		<!-- BEGIN OUTSIDE FORM -->
 		<div id="MainDIV" class="centered">
 			<form id="cl_infos_form" enctype="multipart/form-data" method="POST" action="cl_infos.php"
@@ -541,12 +530,13 @@
 
 				<div name="State" style="opacity: 1; float:left; width: 33%;">
 					<label class="form-els">State</label><br>
-					<select id="cl_state" name="cl_state" onchange='this.form.submit()'>
+					<select id="cl_state" name="cl_state" onchange='StateSelectionSubmission(); this.form.submit()'>
 						<?php
 						if(isset($_POST['cl_stateinits'])) {
 							$cl_stateinits = $_POST['cl_stateinits'];
 						}
 						if(isset($_POST['cl_state'])) {
+							// Second page someone sees
 							$cl_state = $_POST['cl_state'];
 
 
@@ -563,6 +553,7 @@
 
 							echo "<option value=\"" . $cl_state . "\">" . $cl_state . "</option><option value=\"\"></option>";
 						} else {
+							// First time someone sees this page, the state is being selected
 							echo "<option value=\"\">Choose one</option><option value=\"\"></option>";
 						}
 

cl_expenses.php
@@ -1,19 +1,10 @@
 <!DOCTYPE HTML>
 <html>
 	<?php
-	$rundate = date("Y-m-d") . "-" . date("h-i-sa");
-	//echo "<br><br>Now Is . . . ".$rundate."<br><br>";
-	$whoisdis = $_SERVER['REMOTE_USER'];
-	//echo "<br><br>Dis Is . . . ".$whoisdis."<br><br>";
-	$this_log = "./logs/" . $whoisdis . "_" . $rundate . ".txt";
-	$trace_file = fopen($this_log, "w") or die("Unable to open trace file!");
-	$whichreferer = "START-cl_infos\n";
-	fwrite($trace_file, $whichreferer);
-	foreach($_POST as $key => $value) {
-		fwrite($trace_file, $key . "-" . $value . "\n");
-	}
-	fwrite($trace_file, "ESTODO-cl-infos\n");
-	fclose($trace_file);
+
+	require_once 'functions.php';
+	LogPostValuesToLog($_POST['this_log'], 'cl_infos');
+
 	?>
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">

cl_income.php
@@ -1,19 +1,19 @@
 <!DOCTYPE HTML>
 <html>
 	<?php
-	$this_log = $_POST['this_log'];
-	//echo "<br><br>Log Is . . . ".$trace_file."<br>";
-	$rundate = date("Y-m-d") . "-" . date("h-i-sa");
-	//echo "Now Is . . . ".$rundate."<br>";
-	$whoisdis = $_SERVER['REMOTE_USER'];
-	//echo "Dis Is . . . ".$whoisdis."<br><br>";
-	$trace_file = fopen($this_log, "a") or die("Unable to open trace file!");
-	$whichreferer = "START-cl_expenses\n";
-	fwrite($trace_file, $whichreferer);
+	// Generate JSON object for data submitting from previous form (cl_expenses)
+	$output = array();
+	$output['cl_expenses'] = array();
+
 	foreach($_POST as $key => $value) {
-		fwrite($trace_file, $key . "-" . $value . "\n");
+		$output['cl_expenses'][$key] = $value;
 	}
-	fwrite($trace_file, "ESTODO-cl-expenses\n");
+	$output_json = json_encode($output);
+
+	// Write to file on server (get rid of this!)
+	$this_log = $_POST['this_log'];
+	$trace_file = fopen($this_log, "a") or die("Unable to open trace file!");
+	fwrite($trace_file, $output_json);
 	fclose($trace_file);
 	?>
 	<head>

cl_results.php
@@ -2,21 +2,8 @@
 <html>
 	<?php
 	include("config.php");
-	$this_log = $_POST['this_log'];
-	//echo "<br><br>Log Is . . . ".$trace_file."<br>";
-	$rundate = date("Y-m-d") . "-" . date("h-i-sa");
-	//echo "Now Is . . . ".$rundate."<br>";
-	//$whoisdis = $_SERVER['REMOTE_USER'];
-	$whoisdis = "ubuveg";
-	//echo "Dis Is . . . ".$whoisdis."<br><br>";
-	$trace_file = fopen($this_log, "a") or die("Unable to open trace file!");
-	$whichreferer = "START-cl_income\n";
-	fwrite($trace_file, $whichreferer);
-	foreach($_POST as $key => $value) {
-		fwrite($trace_file, $key . "-" . $value . "\n");
-	}
-	fwrite($trace_file, "ESTODO-cl-income\n");
-	fclose($trace_file);
+	require_once 'functions.php';
+	LogPostValuesToLog($_POST['this_log'], 'cl_income');
 	?>
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">

functions.php
@@ -1,0 +1,18 @@
+<?php
+
+// previous_form is the name of the from which this data came
+function LogPostValuesToLog($filename, $previous_form) {
+	$output = array();
+	$output[$previous_form] = array();
+
+	foreach($_POST as $key => $value) {
+		$output[$previous_form][$key] = $value;
+	}
+	$output_json = json_encode($output);
+
+	// Write to file on server (get rid of this!)
+	$trace_file = fopen($filename, "a") or die("Unable to open trace file!");
+	fwrite($trace_file, $output_json);
+	fclose($trace_file);
+}
+?>

commit bf981c16ca2fadec8db6974109127e174cfe7f50

Fix broken data entry
The tool was broken, after selecting the city, and you could no longer
continue.

This was due to two commits: 7fdfc79b77b98389c2091639d801b58179546b09
accidentally got rid of the $ishomeless variable, and then
11807c37442091a532406885eed0c90a6a31fb26 broke logic that depended on
it.

$ishomeless is finally GONE after this commit.

cl_infos.php
@@ -683,38 +683,36 @@
                <hr class=\"org-hr\">
              </div><!-- ABOUT HOME HR --><div id=\"AboutHome\" style=\"opacity: 1; float:left; width: 100%;\">
         <div id=\"AboutHomeLabel\">";
-//				if($ishomeless == "No") {
-//					echo "
-//          <h2 class=\"h2\" style=\"font-weight: normal; font-size: 20px;\">
-//            About your home . . .
-//          </h2>
-//        </div>
-//      </div>
-//
-//    <div id=\"cl_beds\" name=\"Beds\" style=\"opacity: 1; float:left; width: 100%;\">
-//          <label class=\"form-els\">Bedrooms</label><br>
-//          <select id=\"bedrooms\" name=\"bedrooms\" onchange=\"ShowUtilRentMort()\">
-//            <option selected id=\"item9_0_option\" value=\"\">
-//              Choose one
-//            </option>
-//            <option id=\"item9_1_option\" value=\"Efficiency/Studio\">
-//              Efficiency/Studio
-//            </option>
-//            <option id=\"item9_2_option\" value=\"1\">
-//              1
-//            </option>
-//            <option id=\"item9_3_option\" value=\"2\">
-//              2
-//            </option>
-//            <option id=\"item9_4_option\" value=\"3\">
-//              3
-//            </option>
-//            <option id=\"item9_5_option\" value=\"4\">
-//              4
-//            </option>
-//          </select><br><br>
-//        </div>";
-//				}
+					echo "
+          <h2 class=\"h2\" style=\"font-weight: normal; font-size: 20px;\">
+            About your home . . .
+          </h2>
+        </div>
+      </div>
+
+    <div id=\"cl_beds\" name=\"Beds\" style=\"opacity: 1; float:left; width: 100%;\">
+          <label class=\"form-els\">Bedrooms</label><br>
+          <select id=\"bedrooms\" name=\"bedrooms\" onchange=\"ShowUtilRentMort()\">
+            <option selected id=\"item9_0_option\" value=\"\">
+              Choose one
+            </option>
+            <option id=\"item9_1_option\" value=\"Efficiency/Studio\">
+              Efficiency/Studio
+            </option>
+            <option id=\"item9_2_option\" value=\"1\">
+              1
+            </option>
+            <option id=\"item9_3_option\" value=\"2\">
+              2
+            </option>
+            <option id=\"item9_4_option\" value=\"3\">
+              3
+            </option>
+            <option id=\"item9_5_option\" value=\"4\">
+              4
+            </option>
+          </select><br><br>
+        </div>";
 			}
 			?>
 			<div id="cl_utilsmortrent" name="Utilities+Rent/Mort" style="opacity: 1; float:left; width: 100%;">