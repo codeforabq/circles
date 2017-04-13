72361e729b7d234d36f029654a3238dfe2852bdd
Mason Crane <masonecrane@gmail.com>
2017-04-12 19:34:19 -0600

    JSON export and download

--- cl_summary.php
+++ cl_summary.php
@@ -1,7 +1,24 @@
-<!DOCTYPE HTML>
-<html>
 	<?php
 	include("config.php");
+	// Everything that was collected in the previous forms
+	$cl_input = array();
+	foreach($_POST as $key => $value) {
+		$cl_input[$key] = $value;
+	}
+
+	$json = json_encode($cl_input);
+
+	if($_GET['download'] == 1) {
+		header( 'Content-disposition: attachment; filename=test.json' );
+		header( 'Content-type: application/json' );
+		echo $json;
+		exit;
+	}
+	?>
+
+	<!DOCTYPE HTML>
+	<html>
+	<?php
 	if ('Production' != constant('DEPLOY_MODE')) {
 		echo '<h3>Site is running in ' . constant('DEPLOY_MODE') . ': all warnings and errors will be displayed!</h3>';
 		echo "<pre>";
@@ -12,18 +29,12 @@
 	require_once('functions.php');
 	LogPostValuesToLog($_POST['this_log'], 'cl_income');
 
-	// Everything that was collected in the previous forms
-	$cl_input = array();
-	foreach($_POST as $key => $value) {
-		$cl_input[$key] = $value;
-	}
-
 	?>
 	<h1>Summary</h1>
 	<p>Here's all the data you've provided:
 	<pre>
-		<?php var_dump($cl_input); ?>
+		<?php echo $json; ?>
 	</pre>
-	<p><a href="">Click here to download all the data you've provided for offline use.</a></p>
+	<p><a href="?download=1">Click here to download all the data you've provided for offline use.</a></p>
 	<p><a href="javascript:GoResults();">Go to the results page.</a></p>
 </html>
